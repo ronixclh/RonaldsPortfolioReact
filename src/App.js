808ea8de828edf87c681f32bef183217623e1193
Ronalds Borisovs <ronaldsborisovs@MacBook-Air-Ronalds.local>
2023-08-31 13:33:08 +0300

    2023-08-31 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.css'
+
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Contacts from './pages/Contacts'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      {/* <Home /> */}
+      {/* <Projects /> */}
+      <Contacts />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Contacts.js
@@ -0,0 +1,34 @@
+function Contacts() {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <p>Jelgava, Latvija</p>
+          </li>
+
+          <li className="content-list__item">
+            <h2 className="title-2">Telegram / WhatsApp</h2>
+            <p>
+              <a href="tel:+37129828671">+371 29828671</a>
+            </p>
+          </li>
+
+          <li className="content-list__item">
+            <h2 className="title-2">Email</h2>
+            <p>
+              <a href="mailto:ronalds.borisovs@inbox.lv">
+                ronalds.borisovs@inbox.lv
+              </a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  )
+}
+
+export default Contacts
